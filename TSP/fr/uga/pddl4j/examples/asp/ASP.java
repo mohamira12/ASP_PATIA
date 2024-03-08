15dfcd105d0284421a395a54fdec464e0026d60a
Mohammedi <mohammedi.amira12@gmail.com>
2024-03-08 11:14:17 +0100

    ASP

---
+++ TSP/fr/uga/pddl4j/examples/asp/ASP.java
@@ -0,0 +1,67 @@
+package fr.uga.pddl4j.examples.asp;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
+
+import fr.uga.pddl4j.parser.DefaultParsedProblem;
+import fr.uga.pddl4j.plan.Plan;
+import fr.uga.pddl4j.planners.AbstractPlanner;
+import fr.uga.pddl4j.problem.DefaultProblem;
+import fr.uga.pddl4j.problem.Problem;
+import picocli.CommandLine;
+
+/**
+ * The class is an example. It shows how to create a simple A* search planner
+ * able to
+ * solve an ADL problem by choosing the heuristic to used and its weight.
+ *
+ * @author D. Pellier
+ * @version 4.0 - 30.11.2021
+ */
+@CommandLine.Command(name = "ASP", version = "ASP 1.0", description = "Solves a specified planning problem using A* search strategy.", sortOptions = false, mixinStandardHelpOptions = true, headerHeading = "Usage:%n", synopsisHeading = "%n", descriptionHeading = "%nDescription:%n%n", parameterListHeading = "%nParameters:%n", optionListHeading = "%nOptions:%n")
+public class ASP extends AbstractPlanner {
+
+    /**
+     * The class logger.
+     */
+    private static final Logger LOGGER = LogManager.getLogger(ASP.class.getName());
+
+    /**
+     * Instantiates the planning problem from a parsed problem.
+     *
+     * @param problem the problem to instantiate.
+     * @return the instantiated planning problem or null if the problem cannot be
+     *         instantiated.
+     */
+    @Override
+    public Problem instantiate(DefaultParsedProblem problem) {
+        final Problem pb = new DefaultProblem(problem);
+        pb.instantiate();
+        return pb;
+    }
+
+    /**
+     * Search a solution plan to a specified domain and problem using A*.
+     *
+     * @param problem the problem to solve.
+     * @return the plan found or null if no plan was found.
+     */
+    @Override
+    public Plan solve(final Problem problem) {
+    }
+
+    /**
+     * The main method of the <code>ASP</code> planner.
+     *
+     * @param args the arguments of the command line.
+     */
+    public static void main(String[] args) {
+        try {
+            final ASP planner = new ASP();
+            CommandLine cmd = new CommandLine(planner);
+            cmd.execute(args);
+        } catch (IllegalArgumentException e) {
+            LOGGER.fatal(e.getMessage());
+        }
+    }
+}
